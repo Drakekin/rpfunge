b941fa3482388a96b81fc8d3e47969241c8a7ed0
Connor Shearwood <drake@lifein2d.co.uk>
2013-11-19 13:15:33 +0000

    JIT. GLORIOUS JIT.

--- befunge98/__init__.py
+++ befunge98/__init__.py
@@ -3,6 +3,35 @@ from grid import TwoDimensionalLaheySpace as Grid
 from befunge98.instructions import INSTRUCTIONS, move, stack_pop, DEFAULT_INSTRUCTION
 import vec
 
+# So that you can still run this module under standard CPython, I add this
+# import guard that creates a dummy class instead.
+try:
+    from rpython.rlib.jit import JitDriver
+except ImportError:
+    class JitDriver(object):
+        def __init__(self, **kw):
+            pass
+
+        def jit_merge_point(self, **kw):
+            pass
+
+        def can_enter_jit(self, **kw):
+            pass
+
+jit_driver = JitDriver(
+    greens=[
+        'push_mode',
+        'instruction',
+        'position',
+        'velocity',
+        'storage_offset',
+        'program',
+    ], reds=[
+        'stack',
+        'stack_of_stacks'
+    ]
+)
+
 
 def lahey_constrain(position, velocity, program):
     # TODO: Fix the bug whereby if the pointer is outside the program space but pointing towards it, the pointer
@@ -29,13 +58,16 @@ def mainloop(program):
         position = lahey_constrain(position, velocity, program)
         instruction = program.get(position)
         alive = True
-        # self.jitdriver.jit_merge_point(
-        #     velocity=self.velocity,
-        #     location=self.position,
-        #     storage_offset=self.storage_offset,
-        #     stack=self.stack,
-        #     stack_of_stacks=self.stack_of_stacks
-        # )
+        jit_driver.jit_merge_point(
+            push_mode=push_mode,
+            instruction=instruction,
+            position=position,
+            velocity=velocity,
+            storage_offset=storage_offset,
+            program=program,
+            stack=stack,
+            stack_of_stacks=stack_of_stacks
+        )
         if push_mode:
             if instruction == '"':
                 push_mode = False
